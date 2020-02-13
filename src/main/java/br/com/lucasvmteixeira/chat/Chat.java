9d476c4077a16d53d098e484c945f6bb8380aae3
lucas.mteixeira <lucas.mteixeira@rjod275524.prevnet>
2020-02-13 13:45:59 -0300

    criando interface
    Signed-off-by: lucas.mteixeira <lucas.mteixeira@rjod275524.prevnet>

--- src/main/java/br/com/lucasvmteixeira/chat/Chat.java
+++ src/main/java/br/com/lucasvmteixeira/chat/Chat.java
@@ -4,16 +4,36 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Chat {
 
 	public static void main(String[] args) {
-		// Create frame with title Registration Demo
 		JFrame frame = new JFrame();
 		frame.setTitle("Chat");
-
-		// Add panel to frame
-		frame.add(Interface.painelDeChat());
+		
+		final JPanel painelDeAbertura = Interface.construirPainelDeAbertura();
+		painelDeAbertura.setVisible(true);
+		final JPanel painelDeChat = Interface.construirPainelDeChat();
+		painelDeChat.setVisible(false);
+		final JPanel painelDeUsuarios = Interface.construirPainelDeUsuarios();
+		painelDeUsuarios.setVisible(false);
+		
+		final JPanel glassPanel = new JPanel();
+		glassPanel.add(painelDeAbertura);
+		glassPanel.add(painelDeUsuarios);
+		glassPanel.add(painelDeChat);				
+		
+		Interface.btnConectar.addActionListener((evt) -> {
+			painelDeAbertura.setVisible(false);
+			painelDeChat.setVisible(false);
+			painelDeUsuarios.setVisible(true);
+			
+			glassPanel.repaint();
+		});
+		
+		
+		frame.getContentPane().add(glassPanel);
 		frame.pack();
 		frame.setSize(400, 400);
 		frame.setLocationRelativeTo(null);
--- src/main/java/br/com/lucasvmteixeira/chat/Interface.java
+++ src/main/java/br/com/lucasvmteixeira/chat/Interface.java
@@ -1,37 +1,34 @@
 package br.com.lucasvmteixeira.chat;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Font;
 
-import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Interface {
+	public static final JButton btnEnviar = new JButton();
+	public static final JTextField entrada = new JTextField();
 
-	public static JPanel painelDeChat() {
+	public static final JTextField nickname = new JTextField();
+	public static final JButton btnConectar = new JButton();
+
+	public static final JButton btnIniciarChat = new JButton();
+
+	public static final JList<String> usuarios = new JList<String>();
+
+	public static JPanel construirPainelDeChat() {
 		JPanel panel = new JPanel();
 
-		JButton btnEnviar = new JButton();
-		JTextField entrada = new JTextField();
 		JScrollPane jScrollPane1 = new JScrollPane();
 		JTextArea saida = new JTextArea();
 
 		btnEnviar.setText("Enviar");
-		btnEnviar.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				// TODO enviar
-			}
-		});
 
 		jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -42,7 +39,7 @@ public class Interface {
 		saida.setRows(5);
 		jScrollPane1.setViewportView(saida);
 
-		GroupLayout layout = new javax.swing.GroupLayout(panel);
+		GroupLayout layout = new GroupLayout(panel);
 		panel.setLayout(layout);
 		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
 				.createSequentialGroup().addContainerGap()
@@ -65,9 +62,52 @@ public class Interface {
 		return panel;
 	}
 
-	public static JPanel painelDeAbertura() {
-		JPanel mainPanel = new JPanel();
-		
-		return mainPanel;
+	public static JPanel construirPainelDeUsuarios() {
+		JPanel panel = new JPanel();
+
+		btnIniciarChat.setText("Selecionar");
+
+		JScrollPane spane = new JScrollPane();
+		spane.getViewport().add(usuarios);
+
+		JLabel label = new JLabel("Aguirre, der Zorn Gottes");
+		label.setFont(new Font("Serif", Font.PLAIN, 12));
+
+		GroupLayout gl = new GroupLayout(panel);
+		panel.setLayout(gl);
+		gl.setAutoCreateContainerGaps(true);
+		gl.setAutoCreateGaps(true);
+		gl.setHorizontalGroup(gl.createParallelGroup().addComponent(spane)
+				.addGroup(gl.createSequentialGroup().addComponent(label).addComponent(btnIniciarChat)));
+		gl.setVerticalGroup(gl.createSequentialGroup().addComponent(spane)
+				.addGroup(gl.createParallelGroup().addComponent(label).addComponent(btnIniciarChat)));
+
+		return panel;
+	}
+
+	public static JPanel construirPainelDeAbertura() {
+		JPanel panel = new JPanel();
+
+		JLabel label = new JLabel("Digite o seu nome para o chat");
+		btnConectar.setText("Conectar");
+
+		GroupLayout layout = new GroupLayout(panel);
+		panel.setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
+				.createSequentialGroup().addContainerGap()
+				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(label)
+						.addGroup(layout.createSequentialGroup().addComponent(nickname)
+								.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(btnConectar)))
+				.addContainerGap()));
+		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
+				.createSequentialGroup().addContainerGap().addComponent(label)
+				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(nickname)
+						.addComponent(btnConectar, javax.swing.GroupLayout.PREFERRED_SIZE,
+								javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+				.addContainerGap()));
+
+		return panel;
 	}
 }
